62ebdc1b343bcbc639c539587032d58162bfa1e2
The Stoned Raider <joebopie@googlemail.com>
2019-07-11 01:44:20 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config                            = {}
-Config.DrawDistance               = 100.0
---language currently available EN and SV
+
+
 Config.Locale                     = 'en'
 Config.displaymessage               = false
