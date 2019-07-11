8f9a6809db4b935e18dfaabd9b14b81a2a4046b8
The Stoned Raider <joebopie@googlemail.com>
2019-07-11 01:38:29 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,3 @@
-local Keys = {
-  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-}
 
 --- esx
 ESX                           = nil
@@ -18,6 +8,7 @@ Citizen.CreateThread(function ()
     TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
     Citizen.Wait(15)
  	PlayerData = ESX.GetPlayerData()
+	
   end
 end)
 
@@ -36,12 +27,16 @@ AddEventHandler('esx:onPlayerDeath', function(reason)
 	IsDead = true
 end)
 
+
+
+
+
 function WaitforGpress()
 	Citizen.CreateThread(function()
 
 		while IsDead do
 			Citizen.Wait(0)
-			if IsControlPressed(0, Keys['G']) then
+			if IsControlPressed(0, 47) then
 				SendDistressSignals()
 				break
 				
@@ -51,29 +46,34 @@ function WaitforGpress()
 end
 
 function SendDistressSignals()
-	local playerPed = PlayerPedId()
+	local player = PlayerId()
+		local playerPed = PlayerPedId()
 	PedPosition		= GetEntityCoords(playerPed) -- stage 1
-local playerreply = PlayerPedId()		
-TriggerServerEvent('sr_distress',PedPosition,playerreply)
-print(playerreply)
+--local PlayerId = PlayerPedId()	
+local PlayerServerID = GetPlayerServerId(player) 
+TriggerServerEvent('sr_distress',PedPosition,PlayerServerID)
+print"PlayerId"
+print(PlayerId)
+print"PlayerServerID"
+print(PlayerServerID)
+
 end
 
 
-
 RegisterNetEvent('sr_distress')
-AddEventHandler('sr_distress', function(PedPosition,playerreply)   ----- stage 3
+AddEventHandler('sr_distress', function(PedPosition,PlayerServerID)   ----- stage 3
 print"testdistress31"
         local PlayerData = ESX.GetPlayerData(_source)
 --		ESX.PlayerData.job = job
 		  if PlayerData.job ~= nil and (PlayerData.job.name == 'fire' or PlayerData.job.name == 'ambulance' or PlayerData.job.name == 'policeambulance') then
 		  print"testdistress3"
 --sendNotification(_U('Unconcious Citizen'), 'success', 2500)    
-distressmessage(PedPosition,playerreply)	
+distressmessage(PedPosition,PlayerServerID)	
 
 end
 end)
 
-	function distressmessage(PedPosition,playerreply)
+	function distressmessage(PedPosition,PlayerServerID)
 	accepted = false
 --	sendNotification(_U('Unconcious_Citizen'), 'success', 2500)  
 	repeat
@@ -84,14 +84,14 @@ end)
     drawTxt(0.540, 0.69, 1.0,1.0,1.0, "N", 255, 0, 0, 255)
 	drawTxt(0.559, 0.63, 1.0,1.0,1.0, "to accept Call", 255, 100, 100, 255)
     drawTxt(0.559, 0.69, 1.0,1.0,1.0, "to dismiss Call", 255, 100, 100, 255)
-	if IsControlPressed(0, Keys['Y']) then
+	if IsControlPressed(0, 246) then
 	SetNewWaypoint(PedPosition)
 	
-print(playerreply)
-TriggerServerEvent('sr_distressreply',PedPosition,playerreply)
+print(PlayerServerID)
+TriggerServerEvent('sr_distressreply',PedPosition,PlayerServerID)
 	accepted = true
 	end
-	if IsControlPressed(0, Keys['N']) then
+	if IsControlPressed(0, 249) then
 --	SetNewWaypoint(PedPosition)
 	accepted = true
 	end
@@ -122,3 +122,9 @@ function sendNotification(message, messageType, messageTimeout)
 		layout = "bottomCenter"
 	})
 end
+
+
+	
+
+	
+	
